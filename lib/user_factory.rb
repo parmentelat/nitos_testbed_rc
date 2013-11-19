0e5c59fe7002fb2f48832f2800c948de547ba6be
ardadouk <ardadouk@gmail.com>
2013-11-19 20:08:21 +0200

    adding remove user functionallity.

--- lib/user_factory.rb
+++ lib/user_factory.rb
@@ -5,4 +5,9 @@ module OmfRc::ResourceProxy::UserFactory
   include OmfRc::ResourceProxyDSL
 
   register_proxy :user_factory
+
+  configure :deluser do |res, value|
+    cmd = 'userdel -r ' + value[:username]
+    exec cmd
+  end
 end
