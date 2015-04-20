9b7da2840edd30fabaea51c535ef082d7643e555
ardadouk <ardadouk@gmail.com>
2015-04-20 18:08:56 +0300

    image load now fails with a message when the path/image_file does not exist

--- lib/nitos_testbed_rc/frisbeed.rb
+++ lib/nitos_testbed_rc/frisbeed.rb
@@ -41,11 +41,15 @@ module OmfRc::ResourceProxy::Frisbeed
     server.property.image = server.property.image.start_with?('/') ? server.property.image : @fconf[:imageDir] + '/' + server.property.image
     unless File.file?(server.property.image)
       debug "File '#{server.property.image}' does not exist."
-      res.inform(:error, {
-        event_type: 'ERROR',
-        exit_code: -1,
-        msg: "File '#{server.property.image}' does not exist."
-      }, :ALL)
+      Thread.new do
+        sleep 1
+        server.inform(:error, {
+          event_type: 'ERROR',
+          exit_code: -1,
+          msg: "File '#{server.property.image}' does not exist."
+        }, :ALL)
+      end
+      next
     end
     debug "Frisbee server is loading image: #{server.property.image}"
 
